affb6131620a1eace2134983149dd34baa0fd238
absingh <absingh@mpi-cbg.de>
2023-09-25 15:26:01 +0200

    update

--- setup.py
+++ setup.py
@@ -7,28 +7,35 @@ import sys
 
 class InstallOpenFPM(install):
     def run(self):
-        install.run(self)
-        arch = platform.machine()
-        if sys.platform == "darwin":  # macOS
-            print("Please make sure command line tools or Xcode is installed on your system (xcode select --install).")
-            if arch == "arm64":
-                pkg_url = "https://github.com/mosaic-group/openfpm_pdata/releases/download/v4.1.0/openfpm-4.1.0-Darwin-arm64.pkg"
-            elif arch == "x86_64":
-                subprocess.check_call(['brew', 'install', 'gcc@12'])
-                pkg_url = "https://github.com/mosaic-group/openfpm_pdata/releases/download/v4.1.0/openfpm-4.1.0-Darwin-x86_64.pkg"
+        try:
+            arch = platform.machine()
+            if sys.platform == "darwin":  # macOS
+                print("Please make sure command line tools or Xcode is installed on your system (xcode select --install).")
+                if arch == "arm64":
+                    pkg_url = "https://github.com/mosaic-group/openfpm_pdata/releases/download/v4.1.0/openfpm-4.1.0-Darwin-arm64.pkg"
+                elif arch == "x86_64":
+                    subprocess.check_call(['brew', 'install', 'gcc@12'])
+                    pkg_url = "https://github.com/mosaic-group/openfpm_pdata/releases/download/v4.1.0/openfpm-4.1.0-Darwin-x86_64.pkg"
+                else:
+                    print("Unsupported architecture")
+                    sys.exit(1)
+                subprocess.check_call(['wget', '-O', 'bin/openfpm.pkg', pkg_url])
+                subprocess.check_call(['sudo','installer', '-pkg', 'bin/openfpm.pkg', '-target', '/'])
+            elif sys.platform == "linux":  # Linux (assuming Ubuntu for .deb)
+                deb_url = "https://github.com/mosaic-group/openfpm_pdata/releases/download/v4.1.0/openfpm-4.1.0-Linux-x86_64.deb"
             else:
-                print("Unsupported architecture")
+                print("Unsupported platform. We only support macOS and Linux.")
                 sys.exit(1)
-            subprocess.check_call(['wget', '-O', 'bin/openfpm.pkg', pkg_url])
-            subprocess.check_call(['sudo','installer', '-pkg', 'bin/openfpm.pkg', '-target', '/'])
-        elif sys.platform == "linux":  # Linux (assuming Ubuntu for .deb)
-            deb_url = "https://github.com/mosaic-group/openfpm_pdata/releases/download/v4.1.0/openfpm-4.1.0-Linux-x86_64.deb"
-        else:
-            print("Unsupported platform. We only support macOS and Linux.")
+
+            make_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin')
+            subprocess.check_call([f'source /usr/local/openfpm/source/openfpm_vars && make all'],shell=True,cwd=make_dir)
+
+        except Exception as e:
+            print(f"Error during installation of OpenFPM: {e}")
             sys.exit(1)
 
-        make_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin')
-        subprocess.check_call([f'source /usr/local/openfpm/source/openfpm_vars && make all'],shell=True,cwd=make_dir)
+        # Call the parent class's run method at the end
+        install.run(self)
 
 
 setup(
